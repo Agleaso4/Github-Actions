f571d088d642f279043bb787c15fe74fb6f5e73b
agleaso4 <115682702+AmmonArctech@users.noreply.github.com>
2023-10-19 16:01:30 -0600

    fixed the issue with only two arguments rather than 3 for binary

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Main {
     public static void main(String[] args){
         System.out.println("Testing Docker File");
@@ -5,12 +7,16 @@ public class Main {
 
         String calcType = args[0];
         int first = Integer.parseInt(args[1]);
-        int second = Integer.parseInt(args[2]);
+        int second = 0;
+
+        if (args.length > 2 && args[2] != null) {
+            second = Integer.parseInt(args[2]);
+        }
 
         if(calcType.equalsIgnoreCase("add")){
             System.out.println(calculator.add(first, second));
         } else if (calcType.equalsIgnoreCase("subtract")) {
-            System.out.println(calculator.add(first, second));
+            System.out.println(calculator.subtract(first, second));
         } else if (calcType.equalsIgnoreCase("multiply")) {
             System.out.println(calculator.multiply(first, second));
         } else if (calcType.equalsIgnoreCase("binary")){
